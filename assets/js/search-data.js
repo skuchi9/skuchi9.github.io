47069188acc2f95e878f75bb275656577d0970d8
skuchi9 <skuchib@purdue.edu>
2026-02-11 04:37:01 +0000

    Deploying to gh-pages from @ skuchi9/skuchi9.github.io@07d06d55d64f1a4a13b62f95b11b1340f72cefb0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Below is some information about me and my background!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
